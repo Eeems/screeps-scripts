7a054a97af6fd8c8f624636c22424f0169407df8
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2017-12-03 01:23:43 -0700

    remove logging

--- src/bin/initd.ts
+++ src/bin/initd.ts
@@ -1,16 +1,10 @@
 function* sub(): IterableIterator<any>{
-	console.log('Subprocess');
 	yield;
-	console.log('done sub');
 	return 1;
 }
 
 function* run(): IterableIterator<any>{
-	console.log('Main process');
-    let res = yield* sub();
-    console.log('done main');
-    console.log(`result: ${res}`);
-    return res;
+    return yield* sub();
 }
 
 export default run;
