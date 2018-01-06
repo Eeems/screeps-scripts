4bd729984d596533ebbceed0dc4fb20873ca0365
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2018-01-05 22:16:12 -0700

    Get log colour to persist console clears

--- src/log.ts
+++ src/log.ts
@@ -1,31 +1,43 @@
 // All types should be padded to type.length + 2
 // largest type is 'warning' which is 7 characters long.
 const typeCol = _.memoize((name) => `<span class="log ${name}">` + _.padLeft(`${name.toUpperCase()}:`, 9) + '</span>'),
-    indentCol = _.memoize((level) => _.padLeft('', level * 2));
+    indentCol = _.memoize((level) => _.padLeft('', level * 2)),
+    style = JSON.stringify(`
+        span.log{
+            color: white;
+        }
+        span.log.info{
+            background-color: blue;
+        }
+        span.log.debug{
+            background-color: white;
+            color: black;
+        }
+        span.log.warning{
+            background-color: yellow;
+            color: black;
+        }
+        span.log.panic, span.log.error{
+            background-color: red;
+        }
+    `.replace(/\r?\n|\r/g, ''));
 
 let indentLevel = 0;
 
 export class Log{
     public static setup(): void{
-        console.log(`<style>
-            span.log{
-                color: white;
+        console.log(`<script>
+            const style = ${style};
+            let tag = document.getElementById('logging-styles');
+            if(!tag){
+                tag = document.createElement('style');
+                tag.textContent = style;
+                tag.id = 'logging-styles';
+                document.head.append(tag);
+            }else if(tag.textContent != style){
+                tag.textContent = style;
             }
-            span.log.info{
-                background-color: blue;
-            }
-            span.log.debug{
-                background-color: white;
-                color: black;
-            }
-            span.log.warning{
-                background-color: yellow;
-                color: black;
-            }
-            span.log.panic, span.log.error{
-                background-color: red;
-            }
-        </style>`.replace(/\r?\n|\r/g, ''));
+        </script>`.replace(/\r?\n|\r/g, ''));
     }
     public static info(msg: any): void{
         console.log(`${indentCol(indentLevel)}${typeCol('info')} ${msg}`);
