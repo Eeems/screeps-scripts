1e59e632d45f6651e625c7dd98723d88feebea97
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2018-01-06 17:21:17 -0700

    Begin working on documentation for how memory is handled. Begin adding segment and intershard segment handling

---
+++ doc/memory.md
@@ -0,0 +1,16 @@
+Memory global
+=============
+Since Screeps doesn't properly support custom (de)serialization and expects Memory to
+contain certain properties for the rest of it's code to work. We cannot compress all
+of memory. Instead we have to selectivly compress memory as needed. Thus the
+MemoryManager class handles this for us by allowing memory to remain uncompressed and
+storing only part of it compressed via the CompressionManager.
+
+Segments
+========
+Segments are powerful, but due to their nature, you can't have all of them available
+in the same tick. What we can do though is temporarily store segment writes in the
+compressed memory. When its segment is available and has been flushed, it will be
+removed. This is possible since memory is limited to 2MB and each segment is only
+100KB. We will have to be careful to manage our main memory usage, but with proper
+care there will be enough room to properly handle this.
--- src/managers/memory.ts
+++ src/managers/memory.ts
@@ -2,29 +2,54 @@ import {Log} from '../log';
 import * as config from '../config';
 import {CompressionManager as compression} from './compression';
 
+const shards = [
+    'shard0',
+    'shard1',
+    'shard2'
+];
+let Mem: any = {
+        segments: {},
+        activeSegments: [], // limit 10
+        interShardMutations: []
+    },
+    SharedMem: any = {
+        shard: Game.shard.name
+    };
 const EmptyMemory = {
     creeps: {},
     spawns: {},
     flags: {},
     rooms: {},
-    compressed: compression.compress({}, config.memory.format)
+    compressed: compression.compress(Mem, config.memory.format)
 };
 
 export class MemoryManager{
+    public static get segments(): {[id: string]: any}{
+        return Mem.segments;
+    }
+    public static get active(): number[]{
+        return Mem.activeSegments;
+    }
     public static setup(){
         Log.debug('Memory setup');
-        this.loadFromRaw();
+        this.loadCompressed();
         _.defaults(Memory, EmptyMemory)
-        this.saveToRaw();
+        this.flush();
+        this.saveCompressed();
+        this.loadInterShard();
     }
     public static init(){
         Log.debug('Memory init');
-        this.loadFromRaw();
+        this.loadCompressed();
+        this.flush();
+        this.loadInterShard();
     }
     public static deinit(){
         Log.debug('Memory deinit');
+        this.flush();
         this.gc();
-        this.saveToRaw();
+        this.saveCompressed();
+        this.saveInterShard();
     }
     public static reset(): void{
         RawMemory.set(JSON.stringify(EmptyMemory));
@@ -47,18 +72,76 @@ export class MemoryManager{
         return compression.decompress(Memory.compressed);
     }
     public static toString(): string{
-        return compression.compress(Memory.compressed, config.memory.format);
+        return compression.compress(Mem, config.memory.format);
     }
-    private static loadFromRaw(){
+    public static activate(id: number){
+        if(this.active.length < 10){
+            this.active.push(id);
+            return true;
+        }
+        return false;
+    }
+    public static deactivate(id: number){
+        const i = this.active.indexOf(id);
+        if(~i){
+            this.active.splice(i, 1);
+            return true;
+        }
+        return false;
+    }
+    public static interShardSet(prop: string | number, value: any){
+        Mem.interShardMutations.push(['s', prop, value]);
+    }
+    public static interShardDelete(prop: string | number){
+        Mem.interShardMutations.push(['d', prop]);
+    }
+    public static interShardGet(prop: string | number){
+        return SharedMem[prop];
+    }
+    public static interShardHas(prop: string | number){
+        return prop in SharedMem;
+    }
+    private static loadCompressed(){
         try{
-            Memory.compressed = this.toJSON();
+            Mem = this.toJSON();
         }catch(e){
             Log.error(e);
-            Log.panic('Failed to load memory. Resetting');
+            Log.panic('Failed to load compressed memory. Resetting.');
             Memory.compressed = EmptyMemory.compressed;
         }
     }
-    private static saveToRaw(){
+    private static saveCompressed(){
         Memory.compressed = this.toString();
+    }
+    private static loadInterShard(){
+        SharedMem = JSON.parse(RawMemory.interShardSegment);
+        _.each(Mem.interShardMutations, (mutation: any[]) => {
+            const [action, prop] = mutation;
+            if(action === 'd'){
+                delete SharedMem[prop];
+            }else if(action === 's'){
+                SharedMem[prop] = mutation[2];
+            }else{
+                Log.warning(`Invalid intershard mutation: ${action}`);
+            }
+        });
+    }
+    private static saveInterShard(){
+        if(SharedMem.shard === Game.shard.name){
+            const i = shards.indexOf(Game.shard.name) + 1;
+            SharedMem.shard = shards[i < shards.length ? i : 0];
+            RawMemory.interShardSegment = JSON.stringify(SharedMem);
+            Mem.interShardMutations = [];
+        }
+    }
+    private static flush(){
+        _.each(_.keys(this.segments), (id: number) => {
+            if(~this.active.indexOf(id)){
+                RawMemory.segments[id] = JSON.stringify(this.segments[id]);
+                delete this.segments[id];
+            }else if(!this.activate(id)){
+                Log.warning(`Unable to automatically activate segment ${id} for flushing.`);
+            }
+        });
     }
 }
