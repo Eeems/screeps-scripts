002f302d8597f0d42a9af32a70c556ff9b25920c
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2018-01-10 11:51:15 -0700

    Handle cpu bucket being too low

--- src/main.ts
+++ src/main.ts
@@ -7,16 +7,27 @@ Kernel.setup();
 
 export function loop(){
     Log.reset();
-    Log.info('Tick ' + Game.time);
+    Log.info('Tick: ' + Game.time);
     Log.group();
-    try{
-        Kernel.init();
-        Kernel.run();
-        Kernel.deinit();
-    }catch(e){
-        const stack = e.stack || (new Error()).stack;
-        Log.panic(`${e}${stack}`);
-        Kernel.setup();
+    if(Game.cpu.tickLimit < 300){
+        Log.panic('CPU bucket too low. Tick skipped');
+    }else{
+        try{
+            Kernel.init();
+            Kernel.run();
+            Kernel.deinit();
+        }catch(e){
+            const stack = e.stack || (new Error()).stack;
+            Log.panic(`${e}${stack}`);
+            Kernel.setup();
+        }
     }
-    Log.ungroup();
+    // Resetting because we dont know if it errored part way through a group
+    Log.reset();
+    Log.group();
+    Log.info('Statistics');
+    Log.group()
+    Log.info(`Bucket: ${Game.cpu.bucket}`);
+    Log.info(`Usage: ${Game.cpu.getUsed()}`);
+    Log.reset(); // Resetting is quicker than calling ungroup twice
 }
